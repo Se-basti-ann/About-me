cd7314253e5d3db38cf2e8a8a011b993f79bc879
Sebastian Rodriguez Poveda <54719939+Se-basti-ann@users.noreply.github.com>
2022-07-27 22:18:11 -0500

    Add files via upload

---
+++ About-me.js
@@ -0,0 +1,18 @@
+const mi = {
+
+    nombre : 'Sebastian Rodriguez Poveda',
+    edad: 21,
+    desarrollador: true,
+    nacimiento: '24 Julio 2001',
+    libro_favorito: {
+        titulo: 'Tierra sin tiempo',
+        autor:  'Peter Kolosimo',
+        fecha: 1978
+    }
+}
+
+console.log (mi.nombre)
+console.log(mi.edad)
+console.log(mi.desarrollador)
+console.log(mi.nacimiento)
+console.log(mi.libro_favorito)
